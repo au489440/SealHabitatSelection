1cbcde14e7a474dfdd1c562a445aa8bcfd1cb884
au489440 <45428308+au489440@users.noreply.github.com>
2019-01-07 11:01:15 +0100

    Add files via upload

---
+++ C1_Create_workspaces_satdata_fvb.R
@@ -0,0 +1,107 @@
+# Date: 3-Jan-2018
+# Purpose: load seal data and do habitat selection analyses
+# R.3.5.1 64-bits (mac) 
+#			Seems the odbcConnectAccess from the RODBC package only works on 32-bits R (with normal Windows installed). 
+# 			Below some code that works for Mac based on Hmisc and get.mdb()
+
+
+# loading in .mdb files 
+library(Hmisc)
+# Floris: for this to work one should install mdbtools by homebrew: 'brew install mdbtools' (paste into terminal).
+
+# Specify databases
+  databases<-c('pv28', 'pv36', 'pv46', 'hg23', 'hg23f')
+  
+# Specify tables within databases
+  tablenames <- c('gps','tag_info','summary','haulout','dive')
+    
+# Do loop
+  for (database in databases){
+  for (tablename in tablenames){
+  print(database)
+  print(tablename)
+
+# Specify location of databases (NOTE: should be no space in path directory otherwise get.mdb() crashes)
+    channel<- paste("~/Dropbox/Seal_habitat_selection_and_density/rawdat/",database,".mdb",sep="")
+		mdb.get(channel, tables=TRUE)
+
+# Get tables
+#  thetables <- mdb.get(channel, tables=c('gps','tag_info','summary','haulout','dive'))
+
+# Read data and tables into R
+  if (tablename=="gps")  table1<-mdb.get(channel,tables="gps")
+  if (tablename=="tag_info")  table1<-mdb.get(channel,tables="tag_info")
+  if (tablename=="haulout")  table1<-mdb.get(channel,tables="haulout")
+  if (tablename=="summary")  table1<-mdb.get(channel,tables="summary")
+  if (tablename=="dive")  table1<-mdb.get(channel,tables="dive")
+
+# Save as RData
+  if (exists("table1")) save.image(paste("~/Dropbox/Seal_habitat_selection_and_density/workspaces/deployments/",database,tablename,".Rdata",sep=""))
+  
+# remove table1
+  rm("table1")
+
+  }
+  }
+
+
+
+
+
+
+
+
+## original code from Geert
+ 
+
+
+
+
+
+
+
+# Load R3.3.2 32bits
+
+# Make database connection
+  library(RODBC)
+
+# Databases
+  #databases<-c("hg10","hg12","hg14a","hg14g","hg16g","hg21g",
+  #             "pv10","pv16","pv21","pv21b","pv22g","pv25",
+  #             "pv31","pv32","pv37","pv38","pv39")
+  #databases<-c("pv54L","pv54G","pv48","hg41","hg38","hg10","hg43LZ","hg43G","hg43LT")
+  databases<-c("pv62")
+  databases<-c("pv66")
+  
+# Specify table
+  tablenames<-c("gps","diag","summary","haulout","dive")
+    
+# Do loop
+  for (database in databases){
+  for (tablename in tablenames){
+  print(database)
+  print(tablename)
+
+# Load satelite tag table
+  #channel<-odbcConnectAccess(paste("E:/Dropbox/Seal_database/seal/SatDatabases/Finished/",database,".mdb",sep=""))
+  channel<-odbcConnectAccess(paste("E:/Dropbox/Seal_database/seal/SatDatabases/InProgress/",database,".mdb",sep=""))
+  
+# Get tables
+  thetables<-sqlTables(channel)
+
+# Read data_time objects as text
+  if (tablename=="gps" & is.element("gps",thetables$TABLE_NAME))  table1<-sqlFetch(channel,tablename,as.is=3)
+  if (tablename=="diag" & is.element("diag",thetables$TABLE_NAME))  table1<-sqlFetch(channel,tablename,as.is=3)
+  if (tablename=="haulout")  table1<-sqlFetch(channel,tablename,as.is=c(3,4))
+  if (tablename=="summary")  table1<-sqlFetch(channel,tablename,as.is=c(4,5))
+  if (tablename=="dive")  table1<-sqlFetch(channel,tablename,as.is=c(4))
+
+# Save workspace
+  #if (exists("table1")) save.image(paste("w:/imares/texel/zeehonden/seal/workspaces/deployments/",database,tablename,"2015_02_04.rdata",sep=""))
+  if (exists("table1")) save.image(paste("E:/Dropbox/Seal_database/seal/workspaces/deployments/",database,tablename,"2017_11_15.rdata",sep=""))
+  
+# remove table1
+  rm("table1")
+
+  }
+  }
---
+++ C2_Read_Seal_data_fvb.R
@@ -0,0 +1,111 @@
+# LOAD LIBRARIES -------------------------
+  library(maptools)
+  library(rgdal)
+
+# READ TRACKING DATA -------------------
+
+  # Set working directory
+    setwd("~/Dropbox/Seal_habitat_selection_and_density/workspaces/deployments/")
+  
+  # Select GPS tables to read
+    seal.tables<-dir()[grep("gps",dir())] # up to pv62
+    seal.tables
+      
+  # Read in tables and combine    
+    for (i in seal.tables)
+    {
+      print(i)
+      load(i)
+      gps<-table1
+      if(is.element("REF",names(gps))) gps$ref<-gps$REF
+      gps<-gps[is.na(gps$LON)==FALSE & is.na(gps$LAT)==FALSE,]
+       #Floris: changed V_MASK to V.MASK
+       gps<-gps[is.na(gps$V.MASK)==FALSE & gps$V.MASK==0,]
+      #Floris: changed D_DATE to D.DATE
+      gps<-gps[,c("ref","D.DATE","LAT","LON")]
+      if (i==seal.tables[1]) gps.all<-gps
+      else gps.all<-rbind(gps.all,gps)
+    }  
+    gps<-gps.all
+    unique(gps$ref)
+    
+  # Define species
+    gps$species<-tolower(substr(gps$ref,1,2))
+  
+  # calculate correct date
+  #Floris: changed format of date
+    gps$ddate<-as.POSIXct(gps$D.DATE,format="%m/%d/%y %H:%M:%S", tz="UTC")
+  
+  # Sort the data by ref and date
+    gps<-gps[order(gps$ref,gps$ddate),]
+  
+  # Select haulout tables to read
+    haul.tables<-dir()[grep("haulout",dir())] # up to pv62
+    is.element(gsub("gps","haulout",seal.tables),haul.tables)
+    
+  # Read in tables and combine    
+    for (i in haul.tables)
+    {
+      print(i)
+      load(i)
+      haulout<-table1
+      if (is.element("REF",names(haulout))) names(haulout)[match("REF",names(haulout))]<-"ref"
+      if (is.element("species",names(haulout))) haulout<-haulout[,-match("species",names(haulout))]
+      if (is.element("PHOSI_SECS",names(haulout))) haulout<-haulout[,-match("PHOSI_SECS",names(haulout))]
+      if (i==haul.tables[1]) haulout.all<-haulout
+      else haulout.all<-rbind(haulout.all,haulout)
+    }  
+    haulout<-haulout.all
+  
+  # Define species
+    haulout$species<-tolower(substr(haulout$ref,1,2))
+  
+  # calculate correct start and end date
+  #Floris: changed S_DATE to S.DATE 
+  #Floris: changed E_DATE to E.DATE 
+  #Floris: changed format of date
+   haulout$s_ddate<-as.POSIXct(haulout$S.DATE,format="%m/%d/%y %H:%M:%S", tz="UTC")
+    haulout$e_ddate<-as.POSIXct(haulout$E.DATE,format="%m/%d/%y %H:%M:%S", tz="UTC")
+    
+  # Sort the data by ref and date
+    haulout<-haulout[order(haulout$ref,haulout$s_ddate),]
+
+  # Select summary tables to read
+    sum.tables<-dir()[grep("summary",dir())] # up to pv62
+    
+  # Read in tables and combine    
+    for (i in sum.tables)
+    {
+      print(i)
+      load(i)
+      summary<-table1
+      if (is.element("REF",names(summary))) names(summary)[match("REF",names(summary))]<-"ref"
+      if (i==sum.tables[1]) summary.all<-summary
+      else summary.all<-rbind(summary.all[,1:19],summary[,1:19])
+    }  
+    summary<-summary.all
+  
+  # Define species
+    summary$species<-tolower(substr(summary$ref,1,2))
+  
+  # calculate correct start and end date
+   #Floris: changed S_DATE to S.DATE 
+   #Floris: changed E_DATE to E.DATE 
+   #Floris: changed format of date
+	summary$s_ddate<-as.POSIXct(summary$S.DATE,format="%m/%d/%y %H:%M:%S", tz="UTC")
+    summary$e_ddate<-as.POSIXct(summary$E.DATE,format="%m/%d/%y %H:%M:%S", tz="UTC")
+  
+  # Sort the data by ref and date
+    summary<-summary[order(summary$ref,summary$s_ddate),]
+  
+  # Only select seals occuring in gps
+    the.ref<-unique(gps$ref)
+    summary<-summary[is.element(summary$ref,the.ref),]
+    haulout<-haulout[is.element(haulout$ref,the.ref),]
+    
+  # Save the workspace
+    setwd("~/Dropbox/Seal_habitat_selection_and_density")
+    save(gps, file="workspaces/gps.rdata")
+    save(haulout, file="workspaces/haulout.rdata")
+    save(summary,file="workspaces/summary.rdata")
+ 
